04b0dca49e348dfa450074bee45c8b0e578382b2
Jhonatan Medeiros <jhonatan@rodosafra.com>
2024-01-10 06:38:53 -0300

    criando telas, inicio, login, registro, menu e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
